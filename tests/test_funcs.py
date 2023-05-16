5d723c21f75fd1986e4c0562dbfafa6bbfc82eb3
centrisitet <miklekon7@gmail.com>
2023-05-16 21:59:46 +0300

    debugging and add text

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -2,12 +2,9 @@ from utils.funcs import *
 
 import datetime
 import json
-import json
 import pytest
 
-with open('../test.json', 'r', encoding='UTF-8') as f:
-  opers = json.load(f)
-  file = [x for x in opers]
+
 
 
 def test_open_file():
@@ -56,7 +53,7 @@ def test_open_file():
     },
     "description": "Перевод организации",
     "from": "Счет 75106830613657916952",
-    "to": "Счет 11776614605963066702"
+    "to": "Счет 75106830613657916952"
   },
   {
     "id": 587085106,
@@ -88,6 +85,9 @@ def test_open_file():
 
 
 def test_sort_dates():
+    with open('../test.json', 'r', encoding='UTF-8') as f:
+      opers = json.load(f)
+      file = [x for x in opers]
     assert sort_dates(file) == [
       {
     "id": 587085106,
@@ -115,9 +115,35 @@ def test_sort_dates():
     },
     "description": "Перевод организации",
     "from": "Счет 75106830613657916952",
-    "to": "Счет 11776614605963066702"
+    "to": "Счет 75106830613657916952"
+  },      {
+    "id": 594226727,
+    "state": "CANCELED",
+    "date": "12.09.2018",
+    "operationAmount": {
+      "amount": "67314.70",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    }
   },
-        {
+      {
+    "id": 41428829,
+    "state": "EXECUTED",
+    "date": "03.07.2019",
+    "operationAmount": {
+      "amount": "8221.37",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Visa Classic 7158300734726758",
+    "to": "Visa Classic 7158300734726758"
+  },
+  {
     "id": 441945886,
     "state": "EXECUTED",
     "date": "26.08.2019",
@@ -130,37 +156,16 @@ def test_sort_dates():
     },
     "description": "Перевод организации",
     "from": "Maestro 1596837868705199",
-    "to": "Счет 64686473678894779589"
-  },{
-    "id": 594226727,
-    "state": "CANCELED",
-    "date": "12.09.2018",
-    "operationAmount": {
-      "amount": "67314.70",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    }},
-    {
-    "id": 41428829,
-    "state": "EXECUTED",
-    "date": "03.07.2019",
-    "operationAmount": {
-      "amount": "8221.37",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "MasterCard 7158300734726758",
-    "to": "Счет 35383033474447895560"
-  }]
+    "to": "Maestro 1596837868705199"
+  }
+]
 
 
 def test_delete_cancelled():
-        assert delete_cancelled(file) == [
+  with open('../test.json', 'r', encoding='UTF-8') as f:
+    opers = json.load(f)
+    file = [x for x in opers]
+  assert delete_cancelled(file) == [
   {
     "id": 441945886,
     "state": "EXECUTED",
@@ -174,7 +179,7 @@ def test_delete_cancelled():
     },
     "description": "Перевод организации",
     "from": "Maestro 1596837868705199",
-    "to": "Счет 64686473678894779589"
+    "to": "Maestro 1596837868705199"
   },
   {
     "id": 41428829,
@@ -188,8 +193,8 @@ def test_delete_cancelled():
       }
     },
     "description": "Перевод организации",
-    "from": "MasterCard 7158300734726758",
-    "to": "Счет 35383033474447895560"
+    "from": "Visa Classic 7158300734726758",
+    "to": "Visa Classic 7158300734726758"
   },
   {
     "id": 939719570,
@@ -204,7 +209,7 @@ def test_delete_cancelled():
     },
     "description": "Перевод организации",
     "from": "Счет 75106830613657916952",
-    "to": "Счет 11776614605963066702"
+    "to": "Счет 75106830613657916952"
   },
   {
     "id": 587085106,
@@ -224,35 +229,58 @@ def test_delete_cancelled():
 
 
 def test_hide_card_11():
+    with open('../test.json', 'r', encoding='UTF-8') as f:
+      opers = json.load(f)
+      file = [x for x in opers]
     operation = file[0]
     assert hide_card_11(operation)['from'] == "Maestro 1596837******199"
 
 
 def test_hide_card_12():
+    with open('../test.json', 'r', encoding='UTF-8') as f:
+      opers = json.load(f)
+      file = [x for x in opers]
     operation = file[0]
     assert hide_card_12(operation)['to'] == "Maestro 1596837******199"
 
 
 def test_hide_card_21():
+    with open('../test.json', 'r', encoding='UTF-8') as f:
+      opers = json.load(f)
+      file = [x for x in opers]
     operation = file[1]
     assert hide_card_21(operation)['from'] == "Visa Classic 7158300******758"
 
 
 def test_hide_card_22():
+    with open('../test.json', 'r', encoding='UTF-8') as f:
+      opers = json.load(f)
+      file = [x for x in opers]
     operation = file[1]
     assert hide_card_22(operation)['to'] == "Visa Classic 7158300******758"
 
 
 def test_hide_number_from():
+    with open('../test.json', 'r', encoding='UTF-8') as f:
+      opers = json.load(f)
+      file = [x for x in opers]
     operation = file[2]
     assert hide_number_from(operation)['from'] == "Счет **6952"
 
 
 def test_hide_number_to():
+    with open('../test.json', 'r', encoding='UTF-8') as f:
+      opers = json.load(f)
+      file = [x for x in opers]
     operation = file[2]
     assert hide_number_to(operation)['to'] == "Счет **6952"
 
 
 def test_show_operation():
-    assert show_operation(operation = file[0]) == f'{operation["date"]} {operation["description"]}\n{operation["from"]} -> {operation["to"]}\n{operation["operationAmount"]["amount"]} {operation["operationAmount"]["currency"]["name"]}\n'
-    assert show_operation(operation= file[4]) == f'{operation["date"]} {operation["description"]}\n{operation["to"]}\n{operation["operationAmount"]["amount"]} {operation["operationAmount"]["currency"]["name"]}\n'
+    with open('../test.json', 'r', encoding='UTF-8') as f:
+      opers = json.load(f)
+      file = [x for x in opers]
+    operation_1 = file[0]
+    operation_2 = file[3]
+    assert show_operation(operation_1) == f'{operation_1["date"]} {operation_1["description"]}\n{operation_1["from"]} -> {operation_1["to"]}\n{operation_1["operationAmount"]["amount"]} {operation_1["operationAmount"]["currency"]["name"]}\n'
+    assert show_operation(operation_2) == f'{operation_2["date"]} {operation_2["description"]}\n{operation_2["to"]}\n{operation_2["operationAmount"]["amount"]} {operation_2["operationAmount"]["currency"]["name"]}\n'
